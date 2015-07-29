5a7db99b85c14610187e93dc469454ab4cdf1986
brandon <brandon.simpson@thrivemes.com>
2015-07-29 13:25:44 -0400

    update2
    made the multiple if's into one big if lol

--- insert.php
+++ insert.php
@@ -3,96 +3,59 @@ $servername = "mysql16.000webhost.com";
 $username = "a2906519_brandon";
 $password = "zeke2413";
 $database = "a2906519_mydb";
-
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
-
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-
 //check if any fields are blank. If blank field exists kill script and throw error
- if(!isset($first) || trim($first) == '') 
-{
-   echo "First Name Field Empty";
-   die();
-}
-
-if(!isset($last) || trim($last) == '')
-{
-   echo "Last Name Field Empty";
-   die();
-}
-
-if(!isset($birthday) || trim($birthday) == '')
-{
-   echo "Birthday Field Empty";
-   die();
-}
-
-if(!isset($age) || trim($age) == '')
-{
-   echo "Age Field Empty";
-   die();
-}
-
-if(!isset($address) || trim($address) == '')
-{
-   echo "Address Field Empty";
-   die();
-}
 
 
  
-
+ 
 if(isset($_POST['submit']))
-
 {
-
     $file = $_FILES['file']['tmp_name'];
-
     $handle = fopen ($file,"r");
-
    
 
-    while(($fileop = fgetcsv($handle,1000,",")) !==false){
+  if(!isset($first) || trim($first) == '' && !isset($last) || trim($last) == '' && !isset($birthday) || trim($birthday) == '' && !isset($age) || trim($age) == '' && !isset($address) || trim($address) == '' ) 
+{
+   echo "Field empty. Check for empty fields and re-submit";
+   die();
+}
 
+
+
+
+
+    while(($fileop = fgetcsv($handle,1000,",")) !==false){
         $first = $fileop[0];
         $last = $fileop[1];
         $birthday = $fileop[2];
         $age = $fileop[3];
         $address = $fileop[4];
-
 $sql = mysqli_query($conn,"INSERT INTO `mytable` (first, last, birthday, age, address) VALUES ('$first','$last','$birthday','$age','$address')");
 		$getdata =  "SELECT * FROM mytable";
 		$results = mysqli_query($conn,$getdata);
       
-
     }//end while
-
 if(mysqli_num_rows($results) >=1)
  {
  echo "<table><tr><th>First</th><th>Last</th><th>Birthday</th><th>Age</th><th>Address</th></tr>";
 while($row = mysqli_fetch_assoc($results))
 {
 echo "<tr><td>" . $row["first"]. "</td><td>" . $row["last"]. "</td><td>" . $row["birthday"]. "</td><td>" . $row["age"]. "</td><td>" . $row["address"]. "</td></tr>";
-
   
     
 }//end while
 echo "</table>";
     }//end if
-
    
      
-
-
-
 mysqli_close($conn);
 }
-
-
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
