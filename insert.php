94fb61db583e8ac093a7886197c35d9df1d8ad84
brandon <brandon.simpson@thrivemes.com>
2015-08-04 23:41:16 -0400

    update

--- insert.php
+++ insert.php
@@ -1,55 +1,86 @@
-<?php
-include once 'connection.php';
+	
 
+<?php
+
+include 'connection.php';
 
 if(isset($_POST['submit']))
 {
+$empty_value_found = false;
+$file = $_FILES['file']['tmp_name'];
+$handle = fopen ($file,"r");
+$toWrite = array();
+
+while(($fileop = fgetcsv($handle,1000,",")) !==false){
+  
+$first = trim($fileop[0]);
+    $last = trim($fileop[1]);
+    $birthday = trim($fileop[2]);
+    $age = trim($fileop[3]);
+    $address = trim($fileop[4]);
+
+if (($rawData = strptime($birthday, '%m/%d/%G')) !== false) {
+} else {
+  echo 'Wrong date format';
+break;
+}
 
 
 
 
-    $file = $_FILES['file']['tmp_name'];
-    $handle = fopen ($file,"r");
+    if (
+        empty($first) 
+        || empty($last) 
+        || empty($birthday) 
+        || empty($age) 
+        || empty($address) 
+    ) {
+        $empty_value_found = true;
+        echo "empty field please check";
+        break; // stop our while-loop
+    }
+else{
+	$toWrite[] = $fileop;
+
+}
 
 
+}
+
+// now we check - if there no empty values
+if (!$empty_value_found) {
+foreach ($toWrite as $arr){
+	  $first = trim($fileop[0]);
+    $last = trim($fileop[1]);
+    $birthday = trim($fileop[2]);
+    $age = trim($fileop[3]);
+    $address = trim($fileop[4]);
+list ($first, $last, $birthday, $age, $address) = $arr; 
 
 
-    while(($fileop = fgetcsv($handle,1000,",")) !==false){
-
-  $first = $fileop[0];
-        $last = $fileop[1];
-        $birthday = $fileop[2];
-        $age = $fileop[3];
-        $address = $fileop[4];      
-
-	  $sql = mysqli_query($conn,"INSERT INTO `mytable` (first, last, birthday, age, address) VALUES ('$first','$last','$birthday','$age','$address')");
-		$getdata =  "SELECT * FROM mytable";
+ $sql = mysqli_query($conn,"INSERT INTO `mytable` (first, last, birthday, age, address) VALUES ('$first','$last','$birthday','$age','$address')");
+		$getdata =  "SELECT * FROM mytable WHERE first='$first' and last='$last and birthday=$birthday and age='$age' and address='$address'";
 		$results = mysqli_query($conn,$getdata);
-    
-    }//end while
-
-
-
-if(mysqli_num_rows($results) >=1)
+   }
+} 
+  
+      if(mysqli_num_rows($results) >1)
  {
 	
-    
  echo "<table><tr><th>First</th><th>Last</th><th>Birthday</th><th>Age</th><th>Address</th></tr>";
+ }
 while($row = mysqli_fetch_assoc($results))
 {
 echo "<tr><td>" . $row["first"]. "</td><td>" . $row["last"]. "</td><td>" . $row["birthday"]. "</td><td>" . $row["age"]. "</td><td>" . $row["address"]. "</td></tr>";
-  
-    
+
+
 }//end while
 echo "</table>";
-    }//end if
-   
+}
      mysqli_close($conn);
 
-}
+
 ?>
-
-
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
@@ -71,3 +102,5 @@ th{font-family:tahoma; background-color: #CCC; color: white; border: none;}
 
 </body>
 </html>
+
+
